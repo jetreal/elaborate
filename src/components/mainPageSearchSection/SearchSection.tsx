d2315a6d824921d97e83cdaa9d7e58bb98c3a390
Jetreal <bystryi.sokol@gmail.com>
2020-09-06 00:10:57 +0400

    logo section improve

--- src/components/mainPageSearchSection/SearchSection.tsx
+++ src/components/mainPageSearchSection/SearchSection.tsx
@@ -6,15 +6,30 @@ const SearchSection: React.FC = (props) => {
   return (
     <>
       <section className={styles.searchSection}>
+        <div className={styles.ContainerPoint}>
+          <div className={styles.rightSphere}>
+
+          </div>
+          <div className={styles.leftSphere}>
+
+          </div>
+          <div className={styles.leftLine}>
+
+          </div>
+          <div className={styles.rightLine}>
+
+          </div>
+
+        </div>
         <div className={styles.searchSectionPurpleGradient}>
-          
+
         </div>
         <div className={styles.searchSectionLogo}>
           <p>elaborate</p>
         </div>
         <div>
           <h2 className={styles.searchSectionBigText}>
-            Work, Hire, <br/> Collaborate, Invest
+            Work, Hire, <br /> Collaborate, Invest
           </h2>
         </div>
         <p className={styles.searchSectionSecondText}>
