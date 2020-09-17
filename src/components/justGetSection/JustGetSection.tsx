c0f4b36e7a1f50e0a4af205bc57a22834f1d4d7c
jetreal <41300107+jetreal@users.noreply.github.com>
2020-09-17 17:18:30 +0300

    start hand section

--- src/components/justGetSection/JustGetSection.tsx
+++ src/components/justGetSection/JustGetSection.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import styles from './justGetSection.module.sass'
-import imageHands from '../../images/Group238.jpg'
+import imageHands from '../../images/hands2.jpg'
 
 
 const JustGetSection: React.FC<any> = (props: any) => {
     return (
         <div className={styles.justGetSection}>
-          
-          <img className={styles.handsImg}src={imageHands} alt="hands"/>
+          <img className={styles.handsImg} src={imageHands} alt="hands"/>
+          <h3 className={styles.mainHeader}>Just get what<br /> you really need</h3>
         </div>
     )
 }
